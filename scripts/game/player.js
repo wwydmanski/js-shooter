268f66d6871106bc919047c36e2e89f9f55c1d9a
aweas <wwydmanski@gmail.com>
2018-12-15 18:44:42 +0100

    feat: improve gun movement

--- scripts/game/player.js
+++ scripts/game/player.js
@@ -1,11 +1,22 @@
+const speedAddition = 0.4;
+const speedDecay = 0.1;
+
 class Player {
     constructor() {
         this.gunVelocity = new THREE.Vector3();
-        this.delta = 0.8;
+        this.gunMaxOffset = new THREE.Vector3();
+        this.gunMaxOffset.x = [2, 6];
+        this.gunMaxOffset.y = [-4, 0];
+
+
+        this.delta = 1.0;
+        
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
         this.controls = new THREE.PointerLockControls(this.camera);
 
         this.loadGun();
+
+        document.addEventListener('click', this.shoot, true);
     }
 
     loadGun() {
@@ -22,33 +33,35 @@ class Player {
     }
 
     handleHUDMovement(velX, velY, velZ) {
-        player.gun.position.x = 4-velX*0.5;
-        player.gun.position.y = -2-velY*0.1;
-        player.gun.position.z = -4-velZ*0.1;
+        
+        player.gun.position.x = Math.min(Math.max(4-player.gunVelocity.x, player.gunMaxOffset.x[0]), player.gunMaxOffset.x[1]);
+        player.gun.position.y = Math.min(Math.max(-2+player.gunVelocity.y, player.gunMaxOffset.y[0]), player.gunMaxOffset.y[1]);
 
-        player.gun.rotation.x = -velZ*0.115;
-        player.gun.rotation.z = velX*0.115;
+        if(velY!=0 && !player.gunMaxOffset.y.includes(player.gun.position.y))
+            player.gunVelocity.y -= speedAddition * velY*0.1;
+
+        player.gun.rotation.x = -velZ * 0.115;
+        player.gun.rotation.z = velX * 0.115;
+        this.decaySpeed();
+    }
+
+    shoot() {
+        console.log("BOOOM");
     }
 
     handleMouseRotation(movX, movY) {
-        player.delta *= player.delta;
-        player.gunVelocity.x += ( - player.gunVelocity.x ) * 0.08 * player.delta;
-        player.gunVelocity.y += ( - player.gunVelocity.y ) * 0.08 * player.delta;
+        if(movX!=0 && !player.gunMaxOffset.x.includes(player.gun.position.x))
+            player.gunVelocity.x += speedAddition * movX*0.01;
+        if(movY!=0 && !player.gunMaxOffset.y.includes(player.gun.position.y))
+            player.gunVelocity.y += speedAddition * movY*0.01;
+    }
 
-        if(movX > 0 || movY > 0){
-            player.gunVelocity.x += movX*player.delta*0.01;
-            player.gunVelocity.y += movY*player.delta*0.01;
-        }
-        else {
-            player.delta = 0.8;
-            // player.gunVelocity.x += movX*0.005*delta;
-            // player.gunVelocity.y += movY*0.001*delta;
-        }
-
-
-        player.gun.position.x = 4-player.gunVelocity.x;
-        player.gun.position.y = -2-player.gunVelocity.y;
-        player.gun.rotation.z = player.gunVelocity.x;
-        console.info(player.gunVelocity);
+    decaySpeed() {
+        player.gunVelocity.x -= speedDecay * Math.sign(player.gunVelocity.x);
+        player.gunVelocity.y -= speedDecay * Math.sign(player.gunVelocity.y);
+        if (Math.abs(player.gunVelocity.x) <= speedDecay)
+            player.gunVelocity.x = 0;
+        if (Math.abs(player.gunVelocity.y) <= speedDecay)
+            player.gunVelocity.y = 0;
     }
 }
