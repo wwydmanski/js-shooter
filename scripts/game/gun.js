59aea5253c1861a806f96558debaf1ca539a7cb7
aweas <wwydmanski@gmail.com>
2019-01-08 15:00:58 +0100

    feat: shooting direction is based on gun rotation

--- scripts/game/gun.js
+++ scripts/game/gun.js
@@ -14,11 +14,62 @@ class Gun {
             this.geometry = loader_geometry;
 
             this.camera.add(this.geometry);
+
         };
 
         this.loadGun();
 
+        this.makeBall = () => {
+            var rectangle = new Physijs.SphereMesh(
+                new THREE.SphereGeometry(
+                    Math.random() * (4 - 1) + 1,
+                    16,
+                    16
+                ),
+                Physijs.createMaterial(
+                    new THREE.MeshLambertMaterial({
+                        reflectivity: .8,
+                        map: THREE.ImageUtils.loadTexture('plywood.jpg')
+                    }),
+                    .9,
+                    .1
+                ),
+                1
+            );
+    
+            var r = {
+                x: Math.random() * (Math.PI - Math.PI / 12) + Math.PI / 12,
+                y: Math.random() * (Math.PI - Math.PI / 12) + Math.PI / 12,
+                z: Math.random() * (Math.PI - Math.PI / 12) + Math.PI / 12
+            };
+    
+            // var pos = player.controls.getObject().position;
+            var pos = player.gun.localToWorld(new THREE.Vector3());
+            rectangle.rotation.set(r.x, r.y, r.z);
+            rectangle.position.x = pos.x;
+            rectangle.position.y = pos.y;
+            rectangle.position.z = pos.z;
+    
+            rectangle.castShadow = true;
+            rectangle.receiveShadow = true;
+    
+            return rectangle;
+        }
+
+        this.shoot = () => {
+            var ball = this.makeBall();
+            map.addObject(ball);
+
+            var pos = this.localToWorld(new THREE.Vector3());
+
+            var imp_x = -700 * Math.sin(player.controls.getObject().rotation.y);
+            var imp_y = 700 * Math.sin(this.physics.rotation.x);
+            var imp_z = -700 * Math.cos(player.controls.getObject().rotation.y);
+            ball.applyCentralImpulse(new THREE.Vector3(imp_x, imp_y, imp_z));
+        }
+
         this.physics = new GunPhysics(this);
+        document.addEventListener('click', this.shoot, true);
 
         console.info("Gun loaded");
     }
--- scripts/game/player.js
+++ scripts/game/player.js
@@ -8,56 +8,6 @@ class Player {
 
         this.gun = new Gun(this.camera);
 
-        document.addEventListener('click', this.shoot, true);
         console.info("Player loaded");
-    }
-
-    shoot() {
-        var ball = player.makeBall();
-        map.addObject(ball);
-
-        var pos = player.gun.localToWorld(new THREE.Vector3());
-
-        var imp_x = -700*Math.sin(player.controls.getObject().rotation.y);
-        var imp_y = pos.rotation.y;
-        var imp_z = -700*Math.cos(player.controls.getObject().rotation.y);
-        ball.applyCentralImpulse( new THREE.Vector3(imp_x, imp_y, imp_z));
-    }
-
-    makeBall() {
-        var rectangle = new Physijs.SphereMesh(
-            new THREE.SphereGeometry(
-                Math.random() * (4 - 1) + 1,
-                16,
-                16
-            ),
-            Physijs.createMaterial(
-                new THREE.MeshLambertMaterial({
-                    reflectivity: .8,
-                    map: THREE.ImageUtils.loadTexture('plywood.jpg')
-                }),
-                .9,
-                .1
-            ),
-            1
-        );
-
-        var r = {
-            x: Math.random() * (Math.PI - Math.PI / 12) + Math.PI / 12,
-            y: Math.random() * (Math.PI - Math.PI / 12) + Math.PI / 12,
-            z: Math.random() * (Math.PI - Math.PI / 12) + Math.PI / 12
-        };
-        
-        // var pos = player.controls.getObject().position;
-        var pos = player.gun.localToWorld(new THREE.Vector3());
-        rectangle.rotation.set(r.x, r.y, r.z);
-        rectangle.position.x = pos.x;
-        rectangle.position.y = pos.y;
-        rectangle.position.z = pos.z;
-
-        rectangle.castShadow = true;
-        rectangle.receiveShadow = true;
-
-        return rectangle;
     }
 }
