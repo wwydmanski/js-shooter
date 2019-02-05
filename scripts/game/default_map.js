92f204e252a8946209761e8e10dbe658b653f080
aweas <wwydmanski@gmail.com>
2019-02-05 11:19:37 +0100

    feat: add shootable smiley target

--- scripts/game/default_map.js
+++ scripts/game/default_map.js
@@ -1,4 +1,75 @@
 class DefaultMap extends Map {
+    constructor() {
+        super();
+        this.shooting_target;
+        this.shooting_target_material;
+
+        this.material_handler = (materials) => {
+            materials.preload();
+            this.shooting_target_material = materials;
+            this.loadTargets();
+        }
+
+        this.handler = (geometry) => {
+            geometry.scale.set(0.15,0.15,0.15);
+            geometry.position.set(0,1,-150);
+            geometry.rotation.x=-Math.PI/2;
+            var box_container = new Physijs.BoxMesh (
+                new THREE.CubeGeometry( 20, 20, 20 ),
+                this.shooting_target_material,
+                1
+            );
+            // this.shooting_target = geometry;
+            map.scene.add(geometry);
+            console.info("Shooting target loaded");
+        };
+
+        this.loadTargets();
+    }
+
+    loadTargetsMaterial() {
+        var mtlLoader = new THREE.MTLLoader();
+        mtlLoader.load('resources/archery_target.mtl', this.material_handler);
+    }
+    addShape( shape, extrudeSettings, color, x, y, z, rx, ry, rz, s ) {
+        var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+        var mesh = new THREE.Mesh( geometry, 
+            new THREE.MeshPhongMaterial( { color: color } ));
+        var hitbox = new Physijs.CylinderMesh( 
+            new THREE.CylinderGeometry( 41, 41, 10, 32 ), 
+            new THREE.MeshBasicMaterial({ wireframe: true, opacity: 0.5 })
+            ,1 );
+        hitbox.rotation.set(Math.PI/2,0,0);
+        hitbox.scale.set(0.1, 0.1, 0.1);
+        hitbox.add(mesh);
+        hitbox.position.set(0,10,-15);
+        mesh.rotation.set( Math.PI/2,0,0 );
+        mesh.position.set(-40,4,-40);
+        this.scene.add( hitbox );
+    }
+    loadTargets() {
+        var smileyShape = new THREE.Shape();
+        smileyShape.moveTo( 80, 40 );
+        smileyShape.absarc( 40, 40, 40, 0, Math.PI * 2, false );
+        var smileyEye1Path = new THREE.Path();
+        smileyEye1Path.moveTo( 35, 20 );
+        smileyEye1Path.absellipse( 25, 20, 10, 10, 0, Math.PI * 2, true );
+        smileyShape.holes.push( smileyEye1Path );
+        var smileyEye2Path = new THREE.Path();
+        smileyEye2Path.moveTo( 65, 20 );
+        smileyEye2Path.absarc( 55, 20, 10, 0, Math.PI * 2, true );
+        smileyShape.holes.push( smileyEye2Path );
+        var smileyMouthPath = new THREE.Path();
+        smileyMouthPath.moveTo( 20, 40 );
+        smileyMouthPath.quadraticCurveTo( 40, 60, 60, 40 );
+        smileyMouthPath.bezierCurveTo( 70, 45, 70, 50, 60, 60 );
+        smileyMouthPath.quadraticCurveTo( 40, 80, 20, 60 );
+        smileyMouthPath.quadraticCurveTo( 5, 50, 20, 40 );
+        smileyShape.holes.push( smileyMouthPath );
+        var extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+        this.addShape( smileyShape, extrudeSettings, 0xf000f0, 0, 20, -150, 0, 0, Math.PI, 1 );
+    }
+
     createLights() {
         console.info("Creating lights...");
         var light1 = new THREE.DirectionalLight(0xffffff, 1.5);
@@ -37,9 +108,9 @@ class DefaultMap extends Map {
         return mesh;
     }
 
-    createObjects(){
+    createObjects() {
         console.info("Creating objects...");
-        
+
         var geometry = new THREE.BoxGeometry(20, 20, 20);
         var objects = [];
 
@@ -50,7 +121,7 @@ class DefaultMap extends Map {
             face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
             face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
         }
-        
+
         for (var i = 0; i < 500; i++) {
 
             var material = Physijs.createMaterial(new THREE.MeshPhongMaterial({ specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors }));
@@ -61,8 +132,8 @@ class DefaultMap extends Map {
             mesh.position.z = Math.floor(Math.random() * 20 - 10) * 20;
 
             material.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-            mesh.addEventListener('collision', function( other_object ) {  
-                this.material.color.setHex( 0xff0000 );
+            mesh.addEventListener('collision', function (other_object) {
+                this.material.color.setHex(0xff0000);
             });
             objects.push(mesh);
         }
--- scripts/game/player.js
+++ scripts/game/player.js
@@ -2,11 +2,18 @@ const speedAddition = 0.4;
 const speedDecay = 0.1;
 
 class Player {
-    constructor() {
+    constructor(scene) {
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
         this.controls = new THREE.PointerLockControls(this.camera);
 
         this.gun = new Gun(this.camera, this.controls);
+
+        var geometry = new THREE.BoxGeometry(10, 10, 10);
+        var material = Physijs.createMaterial(new THREE.MeshPhongMaterial({ specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors }));
+        var mesh = new Physijs.BoxMesh(geometry, material, 0.1);
+        
+        this.camera.add(mesh);
+        this.mesh = mesh;
 
         console.info("Player loaded");
     }
